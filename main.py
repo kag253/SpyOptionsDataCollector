710d4d951e38a4ff5f2e594f83434f2e5c51f39d
Kristian Gennaci <kristiangennaci@Kristians-MBP.home>
2019-09-10 20:09:30 -0400

    reorganized the file as a class

--- main.py
+++ main.py
@@ -5,154 +5,152 @@ import smtplib
 from datetime import date, timedelta, datetime
 from email.message import EmailMessage
 
+
 ############################################################
 # This simple program retrieves and stores SPY option chains
 # with expirations that are two weeks out.  The program uses
-# tradier for the data.
+# Tradier for the data.
 ############################################################
 
-def create_connection():
-	""" create a database connection to a SQLite database """
 
-	db_file = './options_data.db'
-	try:
-	    conn = sqlite3.connect(db_file)
-	    print(sqlite3.version)
-	except Error as e:
-	    print(e)
+class SpyDataCollector:
 
-	return conn
+	def __init__(self, config_filename):
+		with open(config_filename, 'r') as f:
+			self.config = json.load(f)
 
-def get_tradier_access_token():
-	""" Retrieves the access token needed to make requests to traider's API """
-	config_file = './config.json'
-	with open(config_file, 'r') as f:
-		config = json.load(f)
-		return config['access_token']
+	def create_connection(self):
+		""" create a database connection to a SQLite database """
 
-def get_expiration_dates_to_query():
-	""" 
-		Returns the expiration dates to query for SPY for two weeks out. 
-		SPY has expiration dates every Monday, Wednesday, and Friday, so 
-		this function only returns those dates.
-	"""
+		try:
+		    conn = sqlite3.connect(self.config['db_filepath'])
+		    print(sqlite3.version)
+		except Error as e:
+		    self.send_email('SDC Update: Failed to connect to database', str(e))
 
-	# Monday = 0, Wednesday = 2, Friday = 4
-	expiration_weekday_values = [0, 2, 4] 
+		return conn
 
-	days_out = 14
-	expiration_dates_to_query = []
-	current_date = date.today()
-	for i in range(days_out):
-		current_date = current_date + timedelta(1)
+	def get_expiration_dates_to_query(self):
+		""" 
+			Returns the expiration dates to query for SPY for two weeks out. 
+			SPY has expiration dates every Monday, Wednesday, and Friday, so 
+			this function only returns those dates.
+		"""
 
-		# If the date is a Monday, Wednesday, or Friday, add to list
-		if current_date.weekday() in expiration_weekday_values:
-			expiration_dates_to_query.append(current_date.isoformat())
+		# Monday = 0, Wednesday = 2, Friday = 4
+		expiration_weekday_values = [0, 2, 4] 
 
-	return expiration_dates_to_query
+		days_out = 14
+		expiration_dates_to_query = []
+		current_date = date.today()
+		for i in range(days_out):
+			current_date = current_date + timedelta(1)
 
+			# If the date is a Monday, Wednesday, or Friday, add to list
+			if current_date.weekday() in expiration_weekday_values:
+				expiration_dates_to_query.append(current_date.isoformat())
 
-def send_email(status):
-	""" Sends status emails"""
-
-	# msg = EmailMessage()
-	# msg.set_content('This is a test!')
-	# msg['Subject'] = 'This is a test!'
-	# msg['From'] = 'SPY Data Collector'
-	# msg['To'] = 'kag253@gmail.com'
-
-	# # Send the message via our own SMTP server.
-	# s = smtplib.SMTP('localhost')
-	# s.send_message(msg)
-	# s.quit()
+		return expiration_dates_to_query
 
 
+	def send_email(self, subject, message):
+		""" Sends status emails"""
 
-	mail = smtplib.SMTP('smtp.gmail.com', 587)
-	# server = smtplib.SMTP_SSL
-	# server.login("your username", "your password")
-	mail.sendmail(
-		"blah@address.com", 
-		"kag253@gmail.com", 
-		"this message is from python"
-	)
-	mail.quit()
+		gmail = self.config['email']
+		password = self.config['password']
+		sent_from = gmail
+		to = gmail
+		subject = subject
+		body = message
+		email_text = 'Subject: {}\n\n{}'.format(subject, body)
 
-
-def retrieve_options_data():
-	""" Retrieves the options data from Tradier """
-	
-	access_token = get_tradier_access_token()
-	options_data = []
-	symbol = 'SPY'
-	url = 'https://sandbox.tradier.com/v1/markets/options/chains'
-	expiration_dates_to_query = get_expiration_dates_to_query()
-	for date in expiration_dates_to_query:
-		response = requests.get(url,
-	    	params={'symbol': symbol, 'expiration': date},
-	    	headers={'Authorization': 'Bearer ' + access_token, 'Accept': 'application/json'}
-		)
-		json_response = response.json()
-		if json_response['options']:
-			options_data.extend(json_response['options']['option'])
-		else:
-			# TODO change to email notification
-			print('The ' + date + ' does not have data!')
-			print(json_response)
-
-	return options_data
-
-def shape_options_data(options_data):
-	"""
-		Shapes the raw data into a form acceptable
-		for the database (i.e. remove a lot of unneccesary fields)
-	"""
-
-	quote_timestamp = datetime.now().isoformat()
-	shaped_options_data = []
-
-	for option_obj in options_data:
-		new_option_tuple = (
-			option_obj['symbol'],
-			option_obj['root_symbol'],
-			option_obj['option_type'],
-			option_obj['strike'],
-			option_obj['expiration_date'],
-			quote_timestamp,
-			option_obj['bid'],
-			option_obj['ask'],
-			option_obj['bidsize'],
-			option_obj['asksize']
-		)
-
-		shaped_options_data.append(new_option_tuple)
-	
-	return shaped_options_data
-
-def store_options_data(shaped_data):
-	""" Stores the shaped data into a sqlite database """
-
-	print(shaped_data)
-	insert_sql = 'INSERT INTO options VALUES (?,?,?,?,?,?,?,?,?,?)'
-	try:
-		conn = create_connection()
-		c = conn.cursor()
-		c.executemany(insert_sql, shaped_data)
-		conn.commit()
-		conn.close()
-	except Error as e:
-	    print(e)
-	    # TODO email error
+		try:
+		    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+		    server.ehlo()
+		    server.login(gmail, password)
+		    server.sendmail(sent_from, to, email_text)
+		    server.close()
+		except:
+		    print('Something went wrong...')
+		
 
 
+	def retrieve_options_data(self):
+		""" Retrieves the options data from Tradier """
+		
+		access_token = self.config['access_token']
+		options_data = []
+		symbol = 'SPY'
+		url = 'https://sandbox.tradier.com/v1/markets/options/chains'
+		expiration_dates_to_query = self.get_expiration_dates_to_query()
+		for date in expiration_dates_to_query:
+			response = requests.get(url,
+		    	params={'symbol': symbol, 'expiration': date},
+		    	headers={'Authorization': 'Bearer ' + access_token, 'Accept': 'application/json'}
+			)
+			json_response = response.json()
+			if json_response['options']:
+				options_data.extend(json_response['options']['option'])
+			else:
+				self.send_email('SDC Update: Date has no data', str(date))
 
-def main():
-	data = retrieve_options_data()
-	shaped_data = shape_options_data(data)
-	store_options_data(shaped_data)
+		return options_data
+
+	def shape_options_data(self, options_data):
+		"""
+			Shapes the raw data into a form acceptable
+			for the database (i.e. remove a lot of unneccesary fields)
+		"""
+
+		self.quote_timestamp = datetime.now().isoformat()
+		shaped_options_data = []
+
+		for option_obj in options_data:
+			new_option_tuple = (
+				option_obj['symbol'],
+				option_obj['root_symbol'],
+				option_obj['option_type'],
+				option_obj['strike'],
+				option_obj['expiration_date'],
+				self.quote_timestamp,
+				option_obj['bid'],
+				option_obj['ask'],
+				option_obj['bidsize'],
+				option_obj['asksize']
+			)
+
+			shaped_options_data.append(new_option_tuple)
+		
+		return shaped_options_data
+
+	def store_options_data(self, shaped_data):
+		""" Stores the shaped data into a sqlite database """
+
+		sample_data = shaped_data[0]
+
+		insert_sql = 'INSERT INTO options VALUES (?,?,?,?,?,?,?,?,?,?)'
+		try:
+			conn = self.create_connection()
+			c = conn.cursor()
+			c.executemany(insert_sql, shaped_data)
+			conn.commit()
+			conn.close()
+
+			self.send_email('SDC Update: Data successfully stored for: ' + self.quote_timestamp, str(sample_data))
+		except Error as e:
+			self.send_email('SDC Update: Failed to insert data', str(e))
 
 
-# main()
-send_email('hello')
+	def run(self):
+		data = self.retrieve_options_data()
+		shaped_data = self.shape_options_data(data)
+		self.store_options_data(shaped_data)
+
+
+if __name__== "__main__":
+	config_filename = './config.json'
+	collector = SpyDataCollector(config_filename)
+	collector.run()
+
+
 
